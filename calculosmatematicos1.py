f38fa4fd93314536ab41ed5d100c032a3c60a7c8
Luis Sérgio Barros Marques <46899728+luismarques47@users.noreply.github.com>
2021-07-12 15:46:08 -0300

    Add files via upload

--- calculosmatematicos1.py
+++ calculosmatematicos1.py
@@ -7,17 +7,20 @@ class calculos:
         self.numero = 0
         self.m = 0
         self.n = 0
+        self.x = 0
+        self.y = 0
         self.resultado = 0
         self.opcao = 0
 
     def iniciar(self):
 
-        while(self.opcao != 4):
+        while(self.opcao != 5):
 
             print('1 -  fatorial')
             print('2 -  combinação')
             print('3 -  exponenciação')
-            print('4 -  sair')
+            print('4 -  yˆx')
+            print('5 -  sair')
             self.opcao = int(input('ESCOLHA UMA DAS OPÇÃO ACIMA:'))
 
             if self.opcao == 1:
@@ -27,6 +30,8 @@ class calculos:
             if self.opcao == 3:
                 calculos.exponenciacao()
             if self.opcao == 4:
+                calculos.yelevadox()
+            if self.opcao == 5:
                 exit()
 
     def fatorial(self):
@@ -39,12 +44,17 @@ class calculos:
         self.m = int(input('Entre com o valor para m:'))
         self.n = int(input('Entre com o valor para n:'))
         self.resultado = math.factorial(self.m)/(math.factorial(self.n)*math.factorial(self.m-self.n))
-        print('resultado = ', self.resultado)
+        print('resultado = {:.2f}'.format(self.resultado))
 
 
     def exponenciacao(self):
         self.numero = float(input('Entre com o valor para calcular eˆx:'))
-        print('resultado  = ', math.exp(self.numero))
+        print('resultado  = {:.4f}'.format(math.exp(self.numero)))
+
+    def yelevadox(self):
+        self.y = float(input('Entre com o valor para y:'))
+        self.x = float(input('Entre com o valor para x:'))
+        print('resultado  = {:.4f}'.format(self.y**self.x))
 
 
 calculos = calculos()
